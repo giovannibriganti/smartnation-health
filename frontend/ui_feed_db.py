f9326de03530711b7fcc026b467bfe626aea22c7
Vincent Stragier <vincent.stragier@umons.ac.be>
2024-03-12 21:32:28 +0100

    WIP: extract zipfile in tempdir

--- frontend/ui_feed_db.py
+++ frontend/ui_feed_db.py
@@ -1,6 +1,8 @@
 import os
 import pathlib
+import tempfile
 import uuid
+import zipfile
 
 import streamlit as st
 
@@ -14,23 +16,30 @@ ROOT_PATH = pathlib.Path(__file__).parent
 
 class FeedDb:
     def __init__(self):
-        if "folder_name" not in st.session_state:
-            st.session_state["folder_name"] = str(uuid.uuid4())
+        # if "folder_name" not in st.session_state:
+        #     st.session_state["folder_name"] = str(uuid.uuid4())
 
-        self.folder_name = st.session_state["folder_name"]
-        self.save_path = ROOT_PATH / UPLOAD_FOLDER / self.folder_name
-        self.extract_path = ROOT_PATH / TXT_FOLDER / self.folder_name
-        self.uploaded_local_paths = []
+        # self.folder_name = st.session_state["folder_name"]
+        self.save_path = ROOT_PATH / UPLOAD_FOLDER
+        self.extract_path = ROOT_PATH / TXT_FOLDER
+        # self.uploaded_local_paths = []
 
     def save_uploaded_files(self):
-        self.save_path.mkdir(exist_ok=True, parents=True)
+        # self.save_path.mkdir(exist_ok=True, parents=True)
 
-        for uploaded_file in self.uploaded_files:
-            ext = os.path.splitext(uploaded_file.name)[-1]
-            file_name = self.save_path / (str(uuid.uuid4()) + ext)
-            with file_name.open("wb") as f:
-                f.write(uploaded_file.getbuffer())
-                self.uploaded_local_paths.append(file_name)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            for uploaded_file in self.uploaded_files:
+                
+                subfolder = temp_dir / (str(uuid.uuid4()))
+                subfolder.mkdir(exist_ok=True, parents=True)
+                
+
+                # ext = os.path.splitext(uploaded_file.name)[-1]
+                # file_name = self.save_path / (str(uuid.uuid4()) + ext)
+
+                # with file_name.open("wb") as f:
+                #     f.write(uploaded_file.getbuffer())
+                #     self.uploaded_local_paths.append(file_name)
 
         st.success(
             f"Successfully saved {len(self.uploaded_files)} files to {self.save_path}"
@@ -55,6 +64,7 @@ class FeedDb:
 
     def extract_text(self):
         self.extract_path.mkdir(exist_ok=True, parents=True)
+
         if self.uploaded_local_paths:
             processor = FileProcessor(self.uploaded_local_paths)
             processor.process_files(self.extract_path)
