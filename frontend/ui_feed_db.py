72dab89cf60475c23e4f6cb1277cc2cd04cc9f9a
M. François <mfrancois@fm-voyager>
2024-03-12 22:04:13 +0100

    FEAT: zip uploading

--- frontend/ui_feed_db.py
+++ frontend/ui_feed_db.py
@@ -15,28 +15,29 @@ ROOT_PATH = pathlib.Path(__file__).parent
 
 class FeedDb:
     def __init__(self):
-        self.save_path = ROOT_PATH / UPLOAD_FOLDER
-        self.extract_path = ROOT_PATH / TXT_FOLDER
+        self.save_path = ROOT_PATH / TXT_FOLDER
 
     def save_uploaded_files(self):
 
         with tempfile.TemporaryDirectory() as temp_dir:
+            temp_dir = pathlib.Path(temp_dir)
             for uploaded_file in self.uploaded_files:
 
                 subfolder = temp_dir / (str(uuid.uuid4()))
                 subfolder.mkdir(exist_ok=True, parents=True)
 
                 # extract uploaded zip file inf subfolder
-                with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
+                with zipfile.ZipFile(uploaded_file, "r") as zip_ref:
                     zip_ref.extractall(subfolder)
 
-        st.success(
-            f"Successfully saved {len(self.uploaded_files)} files to {self.save_path}"
-        )
+            st.success(
+                f"Successfully saved {len(self.uploaded_files)} files to {str(temp_dir)}"
+            )
+
+            self.extract_text(temp_dir)
 
     def create_app(self):
-        st.set_page_config(page_icon="📄", layout="wide"
-                           ,page_title="SmartNation")
+        st.set_page_config(page_icon="📄", layout="wide", page_title="SmartNation")
         st.image("assets/logo.png", width=200)
         st.title("Feed Database")
         self.uploaded_files = st.file_uploader(
@@ -49,18 +50,16 @@ class FeedDb:
             if st.button("Create Database"):
                 self.save_uploaded_files()
 
-        """Extract text from uploaded files and create markdown."""
-
     def extract_text(self, temp_dir):
-        self.extract_path.mkdir(exist_ok=True, parents=True)
+        """Extract text from uploaded files and create markdown."""
+        self.save_path.mkdir(exist_ok=True, parents=True)
 
         processor = FileProcessor(temp_dir)
-        processor.process_files(self.extract_path)
+        processor.process_files(self.save_path)
         st.info("Markdown created successfully")
 
-        """Run the app."""
-
     def run(self):
+        """Run the app."""
         self.create_app()
 
 
