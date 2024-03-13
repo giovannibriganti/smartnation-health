2ead5147c6d74812df206a496d0797a123619527
Vincent STRAGIER <vincent.stragier97@gmail.com>
2024-03-13 14:51:55 +0100

    WIP: use unstructured and doc.

--- frontend/utils.py
+++ frontend/utils.py
@@ -1,8 +1,11 @@
-import pathlib
 import logging
-from docx import Document
-from langchain_community.document_loaders import PyPDFLoader, TextLoader
+import pathlib
+import traceback
+
 import streamlit as st
+
+from docx import Document
+from langchain_community.document_loaders import UnstructuredFileLoader
 
 
 class FileProcessor:
@@ -21,32 +24,53 @@ class FileProcessor:
     def __init__(self, root_dir: pathlib.Path):
         self.root_dir = root_dir
 
-    def extract_text_from_pdf(self, pdf_path):
-        loader = PyPDFLoader(pdf_path)
-        pages = loader.load()
-        text = ""
+    def extract_text_from_docx(self, docx_path: pathlib.Path) -> str:
+        """Extract text from a docx file
 
-        for page in pages:
-            text += page.page_content + "\n"
-        return text
+        Args:
+            docx_path (pathlib.Path): Path to the docx file
 
-    def extract_text_from_docx(self, docx_path):
+        Returns:
+            str: Extracted text
+        """
         doc = Document(docx_path)
         text = ""
+
         for paragraph in doc.paragraphs:
             text += paragraph.text + "\n"
+
         return text
 
-    def extract_text_from_txt(self, txt_path):
-        loader = TextLoader(txt_path)
-        text = loader.load()[0].page_content
+    def extract_text_from_unstructured(self, file_path: pathlib.Path) -> str:
+        """Extract text from an unstructured file
+
+        Args:
+            file_path (pathlib.Path): Path to the unstructured file
+
+        Returns:
+            str: Extracted text
+        """
+        loader = UnstructuredFileLoader(file_path)
+        text = loader.load()
         return text
 
-    def save_text(self, text, file, file_name: str):
+    def save_text(self, text: str, file: pathlib.Path, file_name: str) -> None:
+        """Save text to a file
+
+        Args:
+            text (str): Text to save
+            file (pathlib.Path): File to save to
+            file_name (str): Name of the file
+        """
         file.write(f"<FILE>{file_name}</FILE>\n")
         file.write(text)
 
-    def process_files(self, save_path: pathlib.Path):
+    def process_files(self, save_path: pathlib.Path) -> None:
+        """Process files and save to a directory
+
+        Args:
+            save_path (pathlib.Path): Path to save the files
+        """
 
         glob = self.root_dir.glob("**/*")
 
@@ -69,20 +93,28 @@ class FileProcessor:
                     "Patient file exists: %s, overwriting", patient_file.name
                 )
 
-            with patient_file.open("w") as patient_fp:
+            with patient_file.open("w", encoding="utf-8") as patient_fp:
 
                 for file_path in file_list:
-                    if file_path.suffix == ".pdf":
-                        text = self.extract_text_from_pdf(file_path)
-                    elif file_path.suffix == ".docx":
-                        text = self.extract_text_from_docx(file_path)
-                    elif file_path.suffix == ".txt":
-                        text = self.extract_text_from_txt(file_path)
-                    else:
-                        logging.error("Unsupported file type: %s", file_path.name)
-                        st.error(f"Unsupported file type: {file_path.name}")
-                        continue
+                    print("file_path", file_path)
+                    try:
+                        if file_path.suffix.lower() == ".docx":
+                            text = self.extract_text_from_docx(file_path)
 
-                    self.save_text(text, patient_fp, file_path.name)
+                        else:
+                            text = self.extract_text_from_unstructured(
+                                file_path)
+
+                        print("save file", file_path.name,
+                              patient_fp, file_path.name)
+                        self.save_text(text, patient_fp, file_path.name)
+
+                    except Exception:
+                        exec_info = traceback.format_exc()
+                        logging.error(
+                            "Error processing file: %s, %s", file_path.name, exec_info
+                        )
+                        st.error(
+                            f"Error processing file: {file_path.name}, {exec_info}")
 
             logging.info("Processed and saved patient: %s", patient_id)
