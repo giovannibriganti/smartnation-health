97acb145a3ebd6c0a3210948bed2a89996303748
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-04-08 09:38:20 +0200

    Update database.py
    add docstring

--- nosql/app/app/config/database.py
+++ nosql/app/app/config/database.py
@@ -5,9 +5,22 @@ from nosql.app.app.models.patient import Patient
 client =  AsyncIOMotorClient("mongodb://0.0.0.0:27017")
 
 db = client.smartNationAI
-async def init_db():
-    await init_beanie(database = db, document_models=[Patient])
 
+async def init_db():
+    """
+    Initializes the Beanie database connection.
+
+    This function initializes the Beanie connection to the MongoDB database and registers
+    the Patient document model.
+
+    """
+    await init_beanie(database=db, document_models=[Patient])
 
 async def close_db():
+    """
+    Closes the MongoDB connection.
+
+    This function closes the connection to the MongoDB database.
+
+    """
     db.client.close()
