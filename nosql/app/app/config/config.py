8f85564843bb7bb16c224a1cd3dbce47d54fa16b
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-04-08 09:37:20 +0200

    Update config.py

--- nosql/app/app/config/config.py
+++ nosql/app/app/config/config.py
@@ -1,13 +1,29 @@
-import secrets
+# import secrets
 from pydantic.env_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    """
+    Settings class for MongoDB connection.
+
+    Attributes:
+        MONGODB_URL (str): The URL of the MongoDB server.
+        MONGODB_DATABASE (str): The name of the MongoDB database.
+        MONGODB_COLLECTION (str): The name of the MongoDB collection.
+
+    """
+
     MONGODB_URL: str
     MONGODB_DATABASE: str
     MONGODB_COLLECTION: str
 
     class Config:
+        """
+        Configuration class for settings.
+
+        Attributes:
+            case_sensitive (bool): Specifies whether settings are case-sensitive.
+        """
         case_sensitive = True
 
 
